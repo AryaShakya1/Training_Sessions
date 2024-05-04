4c6e9d50b183aac9b0284852207522482ad8194f
AryaShakya1 <arya@ramailo.tech>
2024-05-04 15:20:55 +0545

    Rename for better readability

--- Exercise2.py
+++ Exercise2.py
@@ -3,14 +3,14 @@ list2 = ["y", "me", "s", "lly","i"]
 
 min_length = min(len(list1),len(list2))
 ans=[]
-for i in range(min_length):
-    ans.append(list1[i]+list2[i])
+for index in range(min_length):
+    ans.append(list1[index]+list2[index])
 
 if len(list1) > len(list2):
-    for i in range(min_length,len(list1)):
-        ans.append(list1[i])
+    for index in range(min_length,len(list1)):
+        ans.append(list1[index])
 elif len(list1) < len(list2):
-    for i in range(min_length,len(list2)):
-        ans.append(list2[i])
+    for index in range(min_length,len(list2)):
+        ans.append(list2[index])
 
 print(ans)
