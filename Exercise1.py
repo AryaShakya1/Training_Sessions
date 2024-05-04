1d0c52cf52eefd052d5046264bd0d36849ca3bfe
AryaShakya1 <arya@ramailo.tech>
2024-05-04 15:18:22 +0545

    Add alternative approach

--- Exercise1.py
+++ Exercise1.py
@@ -5,4 +5,10 @@ list1 = [100, 200, 300, 400, 500]
 # list1 = []
 
 reversed_list = list1[::-1]
+
+reversed_list = []
+length = len(list1)
+for index in range(length):
+    reversed_list.append(list1[(length - 1) - index])
+
 print(reversed_list)
